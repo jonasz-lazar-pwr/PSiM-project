2910605032971fdb28b560a6bb329d8c02e93f0f
JonStuPwr <263898@student.pwr.edu.pl>
2024-04-07 11:45:41 +0200

    reload git content

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Dwarf, UserDwarf, Comment, Achievement, UserAchievement
-
-admin.site.register(User)
-admin.site.register(Dwarf)
-admin.site.register(UserDwarf)
-admin.site.register(Comment)
-admin.site.register(Achievement)
-admin.site.register(UserAchievement)
